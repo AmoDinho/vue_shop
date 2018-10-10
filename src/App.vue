e5e6e3c256d8872df9d1ca8983d501d63f938db2
Amo Moloko <u14284783@tuks.co.za>
2018-10-10 12:17:20 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+  <div class="unit" v-for="unit in manifest">
+  <Child :item="unit.item" :url="unit.url"> </Child>
+   </div>
+  </div>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+export default {
+  name: "App",
+  components: {
+    Child
+  },
+  data() {
+    return {
+      manifest: [
+        {
+          item: "Shoe",
+          url: "https://cdn.thesolesupplier.co.uk/2017/11/Yeezy-Boost-350-V2-Semi-Frozen-Yellow-B37572.png"
+        },
+        {
+          item: "T- shirt",
+          url: "https://cdn1.uksoccershop.com/images/arsenal-2014-2015-puma-training-shirt-yellow.jpg"
+        },
+         {
+          item: "Sweatshirt",
+          url: "https://ae01.alicdn.com/kf/HTB1KJY0ePlxYKJjSZFuq6yYlVXaT/Arsenal-1886-Jackets-Men-Pullover-Hoodie-fashion-Sweatshirts-clothing-gunner-kings-of-north-london-is-red.jpg_640x640.jpg"
+        },
+      ]
+    };
+  }
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display:flex;
+  justify-content:center;
+}
+</style>
